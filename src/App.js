8d569e3e3301d3fb854a00a4cadf8f2d971de96f
se-n-pu-u-ki <1023.suzuki.m@gmail.com>
2020-02-06 20:17:21 +0900

    create class components and functional components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-function App() {
-  const dom = <h1>Hello,world</h1>;
-  return (
-    <div className="App">
-      {dom}
-      <input type="text" onClick={() => {console.log("I am a Hero!")}} />
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    const dom = <h1>Hello,world</h1>;
+    return (
+      <div className="App">
+        {dom}
+        <input type="text" onClick={() => {console.log("I am a Hero!")}} />
+      </div>
+    );
+  }
 }
 
 export default App;
